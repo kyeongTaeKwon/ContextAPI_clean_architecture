caae5ebdcaf5de348b3d9f5cb78f9a1fe79055c7
kwon <sjbby100@naver.com>
2020-08-21 00:13:56 +0900

    Add product,productList components

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import { productsItems, Item } from "../fakeData";
+import ProductsList from "../components/productList";
 
 const Products = () => {
   const [items, setItems] = useState<Item[]>(productsItems);
+
   return (
     <div>
-      {items.map(item => (
-        <div>
-          <img src={item.coverImage} alt={item.title} />
-          <h2>{item.title}</h2>
-          <p>{item.price}</p>
-        </div>
-      ))}
+      <ProductsList items={items} />
     </div>
   );
 };
