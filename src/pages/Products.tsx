d8abd57f940a06a26211d29a28d894df9f40a6ac
kwon <sjbby100@naver.com>
2020-08-21 00:32:09 +0900

    Add handlePagination function

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { productsItems, Item } from "../fakeData";
 import ProductsList from "../components/productList";
 
 const Products = () => {
   const [items, setItems] = useState<Item[]>(productsItems);
+  const [currentPage, setPage] = useState<number>(2);
+  const [currentItems, setCurrentItems] = useState<Item[]>([]);
+  const pageSize = 5;
+
+  const handlePagination = () => {
+    const startIndex = (currentPage - 1) * pageSize;
+    let result = items.slice(startIndex, startIndex + pageSize);
+    setCurrentItems(result);
+  };
+
+  useEffect(handlePagination, [currentPage, items]);
 
   return (
     <div>
-      <ProductsList items={items} />
+      <ProductsList items={currentItems} />
     </div>
   );
 };
