178e08ab3a3d4d6885b83c43680d504d032679de
kwon <sjbby100@naver.com>
2020-08-21 00:59:01 +0900

    Add products sort function

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { productsItems, Item } from "../fakeData";
 import ProductsList from "../components/productList";
-
+import _ from "lodash";
 const Products = () => {
   const [items, setItems] = useState<Item[]>(productsItems);
-  const [currentPage, setPage] = useState<number>(2);
+  const [currentPage, setPage] = useState<number>(1);
   const [currentItems, setCurrentItems] = useState<Item[]>([]);
+
   const pageSize = 5;
+  const orderBy = "desc";
 
   const handlePagination = () => {
     const startIndex = (currentPage - 1) * pageSize;
-    let result = items.slice(startIndex, startIndex + pageSize);
+    const result = _.slice(items, startIndex, startIndex + pageSize);
     setCurrentItems(result);
   };
 
+  const handleOrderBy = () => {
+    orderBy === "desc"
+      ? items.sort((a, b) => b.score - a.score)
+      : items.sort((a, b) => a.score - b.score);
+  };
+
+  useEffect(handleOrderBy, [orderBy]);
   useEffect(handlePagination, [currentPage, items]);
 
   return (
