0b3da73877366e9a1630d2cf4266566a63f679aa
kwon <sjbby100@naver.com>
2020-08-24 02:33:25 +0900

    Refactor raise addCart function & hooks to ProductsList component

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,36 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Item } from "../core/data";
-import { ProductBox, ProductImg, ProductPrice, ProductTitle } from "../styles/productStyle/Item";
-import { useProductsState, useProductsDispatch } from "../contexts/ProductsContext";
+import {
+  ProductBox,
+  ProudctImgBox,
+  ProductImg,
+  ProductPrice,
+  ProductTitle,
+  AddCartBtn,
+  AddCartMotionDiv,
+  LinkToCart,
+  AddCartText,
+} from "../styles/productStyle/Item";
 import { makeCommaPrice } from "../core/util/makeComma";
 
 type Props = {
   item: Item;
+  isInCart: boolean;
+  onClick: (isInCart: boolean, item: Item) => number;
 };
 
-const Product = ({ item }: Props) => {
-  const { cart } = useProductsState();
-  const { putCart, takeOutCart } = useProductsDispatch();
+const Product = ({ item, isInCart, onClick }: Props) => {
+  const [animation, setAnimation] = useState<boolean>(false);
+  const handleClick = () => {
+    const cartLength = onClick(isInCart, item);
+    if (!isInCart && cartLength < 3) {
+      setAnimation(true);
+      setTimeout(() => {
+        setAnimation(false);
+      }, 4000);
+    }
+  };
 
-  console.log("프로덕트 렌더링 확인");
   return (
     <ProductBox>
-      {cart.find(el => el.id === item.id) && <p>장바구니에 있습니다.</p>}
-      <ProductImg
-        src={item.coverImage}
-        alt={item.title}
-        onClick={() => {
-          //TODO: 이 부분 클릭시 매번 반복을 돌기 때문에 시간복잡도를 줄일 수 있는 방법 모색해보기 , 하지만 현시점에서는 장바구니 수량이 3개로 제한되어 있어 당장 중요하진 않음!
-
-          if (cart.length < 3) {
-            cart.find(el => el.id === item.id) ? takeOutCart(item.id) : putCart(item);
-          } else {
-            cart.find(el => el.id === item.id) ? takeOutCart(item.id) : alert("장바구니는 3개까지 추가 가능합니다");
-          }
-        }}
-      />
+      <ProudctImgBox>
+        <ProductImg src={item.coverImage} alt={item.title} />
+        <AddCartMotionDiv animate={animation ? "visible" : "hidden"}>
+          <AddCartText>장바구니에 담겼습니다.</AddCartText>
+          <LinkToCart to={"/cart"}>장바구니로 가기</LinkToCart>
+        </AddCartMotionDiv>
+      </ProudctImgBox>
       <ProductTitle>{item.title}</ProductTitle>
       <ProductPrice>{makeCommaPrice(item.price)}원</ProductPrice>
+      <AddCartBtn onClick={handleClick}>{isInCart ? "장바구니에서 빼기" : "장바구니에 담기"}</AddCartBtn>
     </ProductBox>
   );
 };
