b37874c63ad9055991408f6cb411fe1a1b0f2610
kwon <sjbby100@naver.com>
2020-08-24 17:42:58 +0900

    Add cart local storage

--- src/contexts/ProductsContext.tsx
+++ src/contexts/ProductsContext.tsx
@@ -1,19 +1,25 @@
-import React, { createContext, Dispatch, useReducer } from "react";
+import React, { createContext, Dispatch, useReducer, useEffect } from "react";
 import { productsItems, coupons } from "./data";
 import { ProductsReducer, ProductsState, Action } from "./reducers/ProductsReducer";
 
 type ProductsDispatch = Dispatch<Action>;
 
 export const ProductsStateContext = createContext<ProductsState | undefined>(undefined);
-
 export const ProductsDispatchContext = createContext<ProductsDispatch | undefined>(undefined);
 
 export const ProductsContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const localData = localStorage.getItem("cart");
+  const cart = localData ? JSON.parse(localData) : [];
+
   const [products, dispatch] = useReducer(ProductsReducer, {
     items: productsItems,
-    cart: [],
     coupons: coupons,
+    cart,
   });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(products.cart));
+  }, [products.cart]);
 
   return (
     <ProductsDispatchContext.Provider value={dispatch}>
